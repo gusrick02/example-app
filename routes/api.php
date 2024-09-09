2d29bed76c16a43b3f821802848d2f6c88c7cc65
gusrick02 <gustavoricardocarvalho@gmail.com>
2024-09-09 15:38:55 -0300

    primeirasegundaatualizaçao

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 
  Route::apiResource('receita', ReceitaController::class );
  Route::post('/receitas', [ReceitaController::class, 'store']);
+ Route::delete('/receitas/{id}', [ReceitaController::class, 'destroy']);
 
    
    
